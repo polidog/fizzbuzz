4a02b4220c18d657d99288fb20231c60d561b2cd
polidog <polidogs@gmail.com>
2016-09-08 18:43:49 +0900

    inital commit

---
+++ execute.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: polidog
+ * Date: 2016/09/08
+ */
+
+include 'vendor/autoload.php';
+(new \Polidog\FizzBuzz\FizzBuzz())->main();
---
+++ src/FizzBuzz.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: polidog
+ * Date: 2016/09/08
+ */
+
+namespace Polidog\FizzBuzz;
+
+
+class FizzBuzz
+{
+    public function main()
+    {
+        echo "main";
+    }
+}
